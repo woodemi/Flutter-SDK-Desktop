5ddf669c1e3a536b998af69a1e6fb77e2c77b6b1
wangkun <wangkun@woodemi.com>
2020-03-13 07:39:57 +0800

    Impl `create` to `AgoraRtcEngine` on Windows
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- windows/agora_rtc_engine_plugin.cpp
+++ windows/agora_rtc_engine_plugin.cpp
@@ -16,93 +16,107 @@
 // This must be included before VersionHelpers.h.
 #include <windows.h>
 
-#include <VersionHelpers.h>
 #include <flutter/method_channel.h>
+#include <flutter/basic_message_channel.h>
 #include <flutter/plugin_registrar_windows.h>
 #include <flutter/standard_method_codec.h>
 
 #include <map>
 #include <memory>
-#include <sstream>
+
+#include "IAgoraRtcEngine.h"
+
+using namespace agora::rtc;
 
 namespace {
+    using flutter::EncodableMap;
+    using flutter::EncodableValue;
 
-// *** Rename this class to match the windows pluginClass in your pubspec.yaml.
-class AgoraRtcEnginePlugin : public flutter::Plugin {
- public:
-  static void RegisterWithRegistrar(flutter::PluginRegistrarWindows *registrar);
-
-  AgoraRtcEnginePlugin();
-
-  virtual ~AgoraRtcEnginePlugin();
-
- private:
-  // Called when a method is called on this plugin's channel from Dart.
-  void HandleMethodCall(
-      const flutter::MethodCall<flutter::EncodableValue> &method_call,
-      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
-};
-
-// static
-void AgoraRtcEnginePlugin::RegisterWithRegistrar(
-    flutter::PluginRegistrarWindows *registrar) {
-  auto channel =
-      std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
-          registrar->messenger(), "agora_rtc_engine",
-          &flutter::StandardMethodCodec::GetInstance());
-
-  auto plugin = std::make_unique<AgoraRtcEnginePlugin>();
-
-  channel->SetMethodCallHandler(
-      [plugin_pointer = plugin.get()](const auto &call, auto result) {
-        plugin_pointer->HandleMethodCall(call, std::move(result));
-      });
-
-  registrar->AddPlugin(std::move(plugin));
-}
-
-AgoraRtcEnginePlugin::AgoraRtcEnginePlugin() {}
-
-AgoraRtcEnginePlugin::~AgoraRtcEnginePlugin() {}
-
-void AgoraRtcEnginePlugin::HandleMethodCall(
-    const flutter::MethodCall<flutter::EncodableValue> &method_call,
-    std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-  // *** Replace the "getPlatformVersion" check with your plugin's method names.
-  // See:
-  // https://github.com/flutter/engine/tree/master/shell/platform/common/cpp/client_wrapper/include/flutter
-  // and
-  // https://github.com/flutter/engine/tree/master/shell/platform/windows/client_wrapper/include/flutter
-  // for the relevant Flutter APIs.
-  if (method_call.method_name().compare("getPlatformVersion") == 0) {
-    std::ostringstream version_stream;
-    version_stream << "Windows ";
-    // The result returned here will depend on the app manifest of the runner.
-    if (IsWindows10OrGreater()) {
-      version_stream << "10+";
-    } else if (IsWindows8OrGreater()) {
-      version_stream << "8";
-    } else if (IsWindows7OrGreater()) {
-      version_stream << "7";
+    void DebugPrintLine(const std::string& string)
+    {
+        std::wstring wstring{ string.begin(), string.end() };
+        OutputDebugString((wstring + L"\n").c_str());
     }
-    flutter::EncodableValue response(version_stream.str());
-    result->Success(&response);
-  } else {
-    result->NotImplemented();
-  }
-}
 
+    class AgoraRtcEnginePlugin : public flutter::Plugin, IRtcEngineEventHandler
+    {
+    public:
+        static void RegisterWithRegistrar(flutter::PluginRegistrarWindows* registrar);
+
+        AgoraRtcEnginePlugin();
+
+        virtual ~AgoraRtcEnginePlugin();
+
+    private:
+        // Called when a method is called on this plugin's channel from Dart.
+        void HandleMethodCall(
+            const flutter::MethodCall<flutter::EncodableValue>& method_call,
+            std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
+
+        IRtcEngine* agoraRtcEngine;
+    };
+
+    // static
+    void AgoraRtcEnginePlugin::RegisterWithRegistrar(
+        flutter::PluginRegistrarWindows* registrar)
+    {
+        auto plugin = std::make_unique<AgoraRtcEnginePlugin>();
+
+        auto channel =
+            std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
+                registrar->messenger(), "agora_rtc_engine",
+                &flutter::StandardMethodCodec::GetInstance());
+
+        channel->SetMethodCallHandler(
+            [plugin_pointer = plugin.get()](const auto& call, auto result) {
+            plugin_pointer->HandleMethodCall(call, std::move(result));
+        });
+
+        registrar->AddPlugin(std::move(plugin));
+    }
+
+    AgoraRtcEnginePlugin::AgoraRtcEnginePlugin() {}
+
+    AgoraRtcEnginePlugin::~AgoraRtcEnginePlugin()
+    {
+        if (agoraRtcEngine != nullptr)
+            agoraRtcEngine->release();
+        agoraRtcEngine = nullptr;
+    }
+
+    void AgoraRtcEnginePlugin::HandleMethodCall(
+        const flutter::MethodCall<flutter::EncodableValue>& method_call,
+        std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result)
+    {
+        auto methodName = method_call.method_name();
+        auto params = method_call.arguments()->MapValue();
+        DebugPrintLine("plugin HandleMethodCall " + methodName + ", args: " + std::to_string(params.size()));
+
+        if ("create" == methodName)
+        {
+            auto appId = params[EncodableValue("appId")].StringValue();
+            agoraRtcEngine = createAgoraRtcEngine();
+            RtcEngineContext ctx;
+            ctx.eventHandler = this;
+            ctx.appId = appId.c_str();
+            agoraRtcEngine->initialize(ctx);
+            result->Success(nullptr);
+        }
+        else
+            result->NotImplemented();
+    }
 }  // namespace
 
 void AgoraRtcEnginePluginRegisterWithRegistrar(
-    FlutterDesktopPluginRegistrarRef registrar) {
-  // The plugin registrar wrappers owns the plugins, registered callbacks, etc.,
-  // so must remain valid for the life of the application.
-  static auto *plugin_registrars =
-      new std::map<FlutterDesktopPluginRegistrarRef,
-                   std::unique_ptr<flutter::PluginRegistrarWindows>>;
-  auto insert_result = plugin_registrars->emplace(
-      registrar, std::make_unique<flutter::PluginRegistrarWindows>(registrar));
+    FlutterDesktopPluginRegistrarRef registrar)
+{
+    // The plugin registrar wrappers owns the plugins, registered callbacks, etc.,
+    // so must remain valid for the life of the application.
+    static auto* plugin_registrars =
+        new std::map<FlutterDesktopPluginRegistrarRef,
+        std::unique_ptr<flutter::PluginRegistrarWindows>>;
+    auto insert_result = plugin_registrars->emplace(
+        registrar, std::make_unique<flutter::PluginRegistrarWindows>(registrar));
 
-  AgoraRtcEnginePlugin::RegisterWithRegistrar(insert_result.first->second.get());
+    AgoraRtcEnginePlugin::RegisterWithRegistrar(insert_result.first->second.get());
 }
