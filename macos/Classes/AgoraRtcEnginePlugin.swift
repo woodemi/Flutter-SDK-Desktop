b11584b17b795990e4d1fe0c735f8f3719802c73
wangkun <wangkun@woodemi.com>
2020-03-12 22:11:39 +0800

    Impl `create` to `AgoraRtcEngine` on macOS
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- macos/Classes/AgoraRtcEnginePlugin.swift
+++ macos/Classes/AgoraRtcEnginePlugin.swift
@@ -1,19 +1,33 @@
 import Cocoa
 import FlutterMacOS
+import AgoraRtcEngineKit
 
 public class AgoraRtcEnginePlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
+    let plugin = AgoraRtcEnginePlugin()
+
     let channel = FlutterMethodChannel(name: "agora_rtc_engine", binaryMessenger: registrar.messenger)
-    let instance = AgoraRtcEnginePlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
+    registrar.addMethodCallDelegate(plugin, channel: channel)
   }
 
+  private var agoraRtcEngine: AgoraRtcEngineKit?
+
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("macOS " + ProcessInfo.processInfo.operatingSystemVersionString)
+    let method = call.method
+    let params = call.arguments as! Dictionary<String, Any>
+    print("plugin handleMethodCall: \(method), args: \(params)")
+
+    switch method {
+    case "create":
+      let appId = params["appId"] as! String
+      agoraRtcEngine = AgoraRtcEngineKit.sharedEngine(withAppId: appId, delegate: self)
+      result(nil)
     default:
       result(FlutterMethodNotImplemented)
     }
   }
+}
+
+extension AgoraRtcEnginePlugin: AgoraRtcEngineDelegate {
+
 }
