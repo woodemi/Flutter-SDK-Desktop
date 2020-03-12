86f28cfec25b60bef95001b3f37988133f851784
wangkun <wangkun@woodemi.com>
2020-03-12 21:43:18 +0800

    Add `create` to `AgoraRtcEngine`
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,8 @@ import 'dart:async';
 
 import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+
+import 'AppId.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,32 +13,11 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await AgoraRtcEngine.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+    _initAgoraRtcEngine();
   }
 
   @override
@@ -45,12 +25,16 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Agora Flutter SDK'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: Text('TODO'),
         ),
       ),
     );
+  }
+
+  Future<void> _initAgoraRtcEngine() async {
+    AgoraRtcEngine.create(agoraAppId);
   }
 }
--- lib/agora_rtc_engine.dart
+++ lib/agora_rtc_engine.dart
@@ -1,12 +1,14 @@
-import 'dart:async';
-
 import 'package:flutter/services.dart';
 
 class AgoraRtcEngine {
   static const MethodChannel _channel = const MethodChannel('agora_rtc_engine');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  // Core Methods
+  /// Creates an RtcEngine instance.
+  ///
+  /// The Agora SDK only supports one RtcEngine instance at a time, therefore the app should create one RtcEngine object only.
+  /// Only users with the same App ID can join the same channel and call each other.
+  static Future<void> create(String appid) async {
+    await _channel.invokeMethod('create', {'appId': appid});
   }
 }
